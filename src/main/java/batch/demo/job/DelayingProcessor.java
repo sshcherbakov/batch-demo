85c808518791f94dc83683091a850c3e43792576
Sergey Shcherbakov <sshcherbakov@vmware.com>
2013-03-07 11:08:41 +0100

    H2 database support

--- src/main/java/batch/demo/job/DelayingProcessor.java
+++ src/main/java/batch/demo/job/DelayingProcessor.java
@@ -1,21 +1,17 @@
 package batch.demo.job;
 
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import batch.demo.domain.Drawing;
 
 public class DelayingProcessor implements ItemProcessor<Drawing, Drawing> {
 	
-	@Autowired
+	@Value("${batch.demo.processor.delay}")
 	private long delay;
 
 	public Drawing process(Drawing item) throws Exception {
 		Thread.sleep(delay);
 		return item;
-	}
-
-	public void setDelay(long delay) {
-		this.delay = delay;
 	}
 }
