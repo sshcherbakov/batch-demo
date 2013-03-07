3ddf801982a135ed0afb1d4c89cef4a534019083
Stephane Maldini <smaldini@vmware.com>
2013-03-07 14:41:56 +0100

    new configurations and // testing

--- src/test/java/sample/batch/client/JobSyncConfigurationTests.java
+++ src/test/java/sample/batch/client/JobSyncConfigurationTests.java
@@ -10,6 +10,9 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.channel.ChannelInterceptor;
 import org.springframework.integration.channel.QueueChannel;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -17,10 +20,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @ContextConfiguration(locations={"/META-INF/spring/launch-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("default")
+@ActiveProfiles("multithreaded")
 public class JobSyncConfigurationTests {
 	
 	@Autowired
@@ -39,9 +44,34 @@ public class JobSyncConfigurationTests {
 	
 	@Test
 	public void testLaunchJob() throws Exception {
+
+        final int count = 120;
+        final CountDownLatch countDownLatch = new CountDownLatch(count);
+
+        channel.addInterceptor(new ChannelInterceptor() {
+            public Message<?> preSend(Message<?> message, MessageChannel messageChannel) {
+                countDownLatch.countDown();
+                return message;
+            }
+
+            public void postSend(Message<?> message, MessageChannel messageChannel, boolean b) {
+            }
+
+            public boolean preReceive(MessageChannel messageChannel) {
+                return true;
+            }
+
+            public Message<?> postReceive(Message<?> message, MessageChannel messageChannel) {
+                return message;
+            }
+        });
+
 		jobLauncher.run(job, new JobParametersBuilder().addString("batch.demo.input.file",
-                "file:" + System.getProperty("user.dir") + "/LOTTO_ab_2012.csv").addDate("d", new Date()).toJobParameters());
-        Assert.assertTrue(120 == channel.getQueueSize());
+                "file:" + System.getProperty("user.dir") + "/LOTTO_ab_2012_clean.csv").addDate("d", new Date()).toJobParameters());
+
+        countDownLatch.await(30, TimeUnit.SECONDS);
+
+        Assert.assertTrue("Current queue: "+ channel.getQueueSize(), count == channel.getQueueSize());
 	}
 	
 }
