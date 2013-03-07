5eb27bf5e93e41b352d048114d1c353d807e0440
Sergey Shcherbakov <sshcherbakov@vmware.com>
2013-03-07 13:43:39 +0100

    Job execution time measuring listener added

--- src/main/java/batch/demo/job/ExecutionTimeMeasurer.java
+++ src/main/java/batch/demo/job/ExecutionTimeMeasurer.java
@@ -1,17 +1,28 @@
 package batch.demo.job;
 
+import org.apache.log4j.Logger;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.annotation.AfterJob;
+import org.springframework.batch.core.annotation.BeforeJob;
 
 import com.jamonapi.Monitor;
+import com.jamonapi.MonitorFactory;
 
-public class ExecutionTimeMeasurer implements JobExecutionListener {
+public class ExecutionTimeMeasurer {
+	
+	private static Logger log = Logger.getLogger(ExecutionTimeMeasurer.class);
+	
+	private Monitor mon;
 
+	@BeforeJob
 	public void beforeJob(JobExecution jobExecution) {
+		mon = MonitorFactory.start();
 	}
 
+	@AfterJob
 	public void afterJob(JobExecution jobExecution) {
-
+		mon.stop();
+		log.info(mon);
 	}
 
 }
