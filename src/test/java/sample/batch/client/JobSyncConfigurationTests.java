eb14d3b7ed6c3cad3c4f333b17d5fa66c0134399
Stephane Maldini <smaldini@vmware.com>
2013-03-07 14:07:19 +0100

    add JMS queue output

--- src/test/java/sample/batch/client/JobSyncConfigurationTests.java
+++ src/test/java/sample/batch/client/JobSyncConfigurationTests.java
@@ -2,6 +2,7 @@ package sample.batch.client;
 
 import static org.junit.Assert.assertNotNull;
 
+import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
@@ -9,9 +10,13 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 @ContextConfiguration(locations={"/META-INF/spring/launch-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,6 +28,9 @@ public class JobSyncConfigurationTests {
 
 	@Autowired
 	private Job job;
+
+    @Resource(name="outputChannel")
+    private QueueChannel channel;
 	
 	@Test
 	public void testSimpleProperties() throws Exception {
@@ -32,7 +40,8 @@ public class JobSyncConfigurationTests {
 	@Test
 	public void testLaunchJob() throws Exception {
 		jobLauncher.run(job, new JobParametersBuilder().addString("batch.demo.input.file",
-                "file:"+System.getProperty("user.dir")+"/LOTTO_ab_2012.csv").toJobParameters());
+                "file:" + System.getProperty("user.dir") + "/LOTTO_ab_2012.csv").addDate("d", new Date()).toJobParameters());
+        Assert.assertTrue(120 == channel.getQueueSize());
 	}
 	
 }
