1715c81d10aaa07c59917347e21475683928fd14
Stephane Maldini <smaldini@vmware.com>
2013-03-07 12:02:05 +0100

    change structure

--- src/test/java/sample/batch/client/JobSyncConfigurationTests.java
+++ src/test/java/sample/batch/client/JobSyncConfigurationTests.java
@@ -6,14 +6,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration(locations={"/launch-context.xml"})
+@ContextConfiguration(locations={"/META-INF/spring/launch-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
-public class JobConfigurationTests {
+@ActiveProfiles("default")
+public class JobSyncConfigurationTests {
 	
 	@Autowired
 	private JobLauncher jobLauncher;
@@ -28,7 +31,8 @@ public class JobConfigurationTests {
 	
 	@Test
 	public void testLaunchJob() throws Exception {
-		jobLauncher.run(job, new JobParameters());
+		jobLauncher.run(job, new JobParametersBuilder().addString("batch.demo.input.file",
+                "file:"+System.getProperty("user.dir")+"/LOTTO_ab_2012.csv").toJobParameters());
 	}
 	
 }
